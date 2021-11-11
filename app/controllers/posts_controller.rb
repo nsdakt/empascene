01cc90ceea83b3fcf436df929683d75d14a6a1b6
nsdakt <dnj.0077@gmail.com>
2021-11-11 05:48:40 +0000

    [add]post/index_new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
     @post.user_id = current_user.id
     if @post.save
       flash[:success] = "投稿しました！"
-    　redirect_to posts_path
+      redirect_to posts_path
     else
       render :new
     end
