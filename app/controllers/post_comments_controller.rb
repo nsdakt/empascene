f276af443f6154d1b4f6078d90ff1e56dbeb880d
nsdakt <dnj.0077@gmail.com>
2021-11-14 18:18:18 +0000

    [add]ajax/fav_comment

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,16 +1,18 @@
 class PostCommentsController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    comment = PostComment.new(post_comment_params)
-    comment.user_id = current_user.id
-    comment.post_id = post.id
-    comment.save
-    redirect_to post_path(post)
+    @post = Post.find(params[:post_id])
+    @post_comment = PostComment.new(post_comment_params)
+    @post_comment.user_id = current_user.id
+    @post_comment.post_id = post.id
+    @post_comment.save
+    # app/views/book_comments/create.js.erbを参照
   end
 
   def destroy
-    PostComment.find_by(id: params[:id]).destroy
-    redirect_to post_path(params[:post_id])
+    @post = Post.find(params[:book_id])
+    post_coment = @post.post_coments.find(params[:id])
+    post_coment.destroy
+    # app/views/book_comments/destroy.js.erbを参照
   end
 
   private
