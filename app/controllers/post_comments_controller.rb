8b75e4ca70207a005e3a39db46900f3482c48455
nsdakt <dnj.0077@gmail.com>
2021-11-15 20:30:16 +0000

    [add]ajax_commments/gem_pryrails

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -3,16 +3,16 @@ class PostCommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @post_comment = PostComment.new(post_comment_params)
     @post_comment.user_id = current_user.id
-    @post_comment.post_id = post.id
+    @post_comment.post_id = @post.id
     @post_comment.save
-    # app/views/book_comments/create.js.erbを参照
+    # app/views/post_comments/create.js.erbを参照
   end
 
   def destroy
-    @post = Post.find(params[:book_id])
-    post_coment = @post.post_coments.find(params[:id])
-    post_coment.destroy
-    # app/views/book_comments/destroy.js.erbを参照
+    @post = Post.find(params[:post_id])
+    post_comment = @post.post_comments.find(params[:id])
+    post_comment.destroy
+    # app/views/post_comments/destroy.js.erbを参照
   end
 
   private
